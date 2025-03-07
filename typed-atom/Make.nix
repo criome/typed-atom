8de64ee982e960eb62d12d67c8e485481973d465
li <li@maisiliym.criome.me>
2025-03-07 13:26:43 +0100

    renamed(mkTypedAtom make)

--- typed-atom/Make.nix
+++ typed-atom/Make.nix
@@ -3,11 +3,13 @@ atom@{
   ...
 }:
 let
-  implementations = {
-    rust = get.rust-atom;
-  };
+  implementations = [ "rust-atom" ];
+  implementationExists = std.elem type implementations;
 
   missingImplementationError = abort "Missing implementation for atom type: ${type}";
 
+  maker = get.${type}.make;
+
 in
-{ }
+assert implementationExists || missingImplementationError;
+maker atom
